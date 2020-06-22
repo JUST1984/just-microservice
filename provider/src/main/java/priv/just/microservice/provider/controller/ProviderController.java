b869d81ac53bb035198add5cc8f9fb6a3542aa07
JUST1984 <yixiezi1994@gmail.com>
2020-06-22 21:13:59 +0800

    update

--- provider/src/main/java/priv/just/microservice/provider/controller/ProviderController.java
+++ provider/src/main/java/priv/just/microservice/provider/controller/ProviderController.java
@@ -2,6 +2,8 @@ package priv.just.microservice.provider.controller;
 
 import com.alibaba.fastjson.JSONObject;
 import lombok.extern.slf4j.Slf4j;
+import org.springframework.cloud.context.config.annotation.RefreshScope;
+import org.springframework.core.env.Environment;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
@@ -9,12 +11,17 @@ import priv.just.microservice.provider.api.domain.ProviderReq;
 import priv.just.microservice.provider.api.domain.ProviderResp;
 import priv.just.microservice.provider.api.service.ProviderService;
 
+import javax.annotation.Resource;
 import java.util.HashMap;
 import java.util.Map;
 
 @Slf4j
 @RestController
+// @RefreshScope
 public class ProviderController implements ProviderService {
+
+    @Resource
+    private Environment environment;
 
     @Override
     @RequestMapping("invoke")
@@ -25,6 +32,11 @@ public class ProviderController implements ProviderService {
         Map<String, Object> body = new HashMap<>();
         res.setBody(body);
         return res;
+    }
+
+    @RequestMapping("getProperty")
+    public String getProperty(String name) {
+        return environment.getProperty(name);
     }
 
 }
